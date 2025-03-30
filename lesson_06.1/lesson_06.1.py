ad17e27b0882b561b56f629a3bf214871eafca4a
nastasiaKN <anastasiyakiyasha@gmail.com>
2025-03-30 18:55:42 +0300

    added solution

--- lesson_06.1/lesson_06.1.py
+++ lesson_06.1/lesson_06.1.py
@@ -2,5 +2,11 @@
 # Якщо їх більше 10 - вивести в консоль True, інакше - False.
 # Строку отримати за допомогою функції input()
 
-my_phrase = input("input you phrase")
-print(my_phrase)
+my_phrase = input("input you phrase: ")
+unique_symbols = set(my_phrase)
+
+if len(unique_symbols) > 10:
+    print(True)
+else:
+    print(False)
+
