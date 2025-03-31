af6a3873bd4371459e761785cd7e3bae27afdffc
nastasiaKN <anastasiyakiyasha@gmail.com>
2025-03-31 15:37:15 +0300

    lesson 6.2

--- lesson_6.2/lesson_6.2.py
+++ lesson_6.2/lesson_6.2.py
@@ -1,3 +1,16 @@
 # Напишіть цикл, який буде вимагати від користувача ввести слово,
 # в якому є літера "h" (враховуються як великі так і маленькі).
 # Цикл не повинен завершитися, якщо користувач ввів слово без букви "h".
+
+my_input = input('Please enter words: "Happy hamster": ')
+
+found = False
+for i in my_input:
+    if i == "h" or i == "H":
+        found = True
+        break
+if found:
+    print("You are right")
+else:
+    print("You are wrong")
+
