35f93dffad9b79f54aad6a801a0604ab2f74c3ce
nastasiaKN <anastasiyakiyasha@gmail.com>
2025-04-01 18:38:43 +0300

    fixed. added while loop

--- lesson_6.2/lesson_6.2.py
+++ lesson_6.2/lesson_6.2.py
@@ -2,15 +2,20 @@
 # в якому є літера "h" (враховуються як великі так і маленькі).
 # Цикл не повинен завершитися, якщо користувач ввів слово без букви "h".
 
-my_input = input('Please enter words: "Happy hamster": ')
+my_input_char = input('Please enter words with h/H: ')
+while 'h' not in my_input_char.lower():
+    my_input_char = input('Please enter words with h/H: ')
 
-found = False
-for i in my_input:
-    if i == "h" or i == "H":
-        found = True
-        break
-if found:
-    print("You are right")
-else:
-    print("You are wrong")
+print(my_input_char)
+print("You are right")
+
+
+
+
+
+
+
+
+
+
 
