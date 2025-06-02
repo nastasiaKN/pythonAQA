f2dea1db71ef063c4f8318c1b215e22f6844e105
nastasiaKN <anastasiyakiyasha@gmail.com>
2025-06-02 17:05:09 +0200

    added str method

--- lesson_14.1/lesson_14.1.py
+++ lesson_14.1/lesson_14.1.py
@@ -8,15 +8,13 @@ class Student:
     def update_avg_marks(self, new_mark):
         self.avg_marks = new_mark
 
-    def show_info(self):
-        print("Name:", self.first_name)
-        print("Last name:", self.last_name)
-        print("Age:", self.age)
-        print("Average marks:", self.avg_marks)
+    def __str__(self):
+        return (f"Name: {self.first_name}\n"
+                f"Last name: {self.last_name}\n"
+                f"Age: {self.age}\n"
+                f"Average marks: {self.avg_marks}")
 
-
-student1 = Student("John", "Doe", 18, 4.2)
-
-student1.update_avg_marks(4.8)
-
-student1.show_info()
+if __name__ == "__main__":
+    student1 = Student("John", "Doe", 18, 4.2)
+    student1.update_avg_marks(4.8)
+    print(student1)
